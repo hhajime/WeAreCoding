4390342cf075010d6910635346748f041264ba56
Hajime <impapa68@gmail.com>
2023-11-07 12:29:48 +0900

    add: SampleApp

---
+++ CozySwiftUIWithJudy/SampleApp/SampleApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SampleApp
+//
+//  Created by Ha Jong Myeong on 11/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CozySwiftUIWithJudy/SampleApp/SampleApp/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  SampleApp
+//
+//  Created by Ha Jong Myeong on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
